21113742cb7263f9cc90c1d7f3501ce31cafd437
Jasdeep Singh <j.singhhh917@gmail.com>
2025-03-03 17:28:45 +1100

    fetch

--- app.js
+++ app.js
@@ -15,26 +15,42 @@
 // firstReq.send();
 
 // the same code as above but nested as some of the arrays had links to another page such as movie titles. So you have to next another XML request in the first one
-const firstReq = new XMLHttpRequest();
-firstReq.addEventListener('load', function() {
-	console.log('FIRST REQUEST WORKED!!!');
-	const data = JSON.parse(this.responseText);
-	const filmURL = data.results[0].films[0];
-	const filmReq = new XMLHttpRequest();
-	filmReq.addEventListener('load', function() {
-		console.log('SECOND REQUEST WORKED!!!');
-		const filmData = JSON.parse(this.responseText);
-		console.log(filmData.title);
-	});
-	filmReq.addEventListener('error', function(e) {
-		console.log('ERROR!!', e);
-	});
-	filmReq.open('GET', filmURL);
-	filmReq.send();
-});
-firstReq.addEventListener('error', (e) => {
-	console.log('ERROR!!!!!!');
-});
-firstReq.open('GET', 'https://swapi.co/api/planets/');
-firstReq.send();
-console.log('Request Sent!');
+// const firstReq = new XMLHttpRequest();
+// firstReq.addEventListener('load', function() {
+// 	console.log('FIRST REQUEST WORKED!!!');
+// 	const data = JSON.parse(this.responseText);
+// 	const filmURL = data.results[0].films[0];
+// 	const filmReq = new XMLHttpRequest();
+// 	filmReq.addEventListener('load', function() {
+// 		console.log('SECOND REQUEST WORKED!!!');
+// 		const filmData = JSON.parse(this.responseText);
+// 		console.log(filmData.title);
+// 	});
+// 	filmReq.addEventListener('error', function(e) {
+// 		console.log('ERROR!!', e);
+// 	});
+// 	filmReq.open('GET', filmURL);
+// 	filmReq.send();
+// });
+// firstReq.addEventListener('error', (e) => {
+// 	console.log('ERROR!!!!!!');
+// });
+// firstReq.open('GET', 'https://swapi.co/api/planets/');
+// firstReq.send();
+// console.log('Request Sent!');
+
+
+// same code as above but written using fetch
+fetch('https://swapi.co/api/planets/').then((response) => {
+  if (!response.ok)
+    throw new Error(`Status Code Error: ${response.status}`);
+  response.json().then((data) => {
+    for(let planet of data.results) {
+      console.log(planet.name);
+    }
+  })
+})
+.catch((err) => {
+  console.log('SOMETHING WENT WRONG');
+  console.log(err);
+})
