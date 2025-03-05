ee910baabcf4fa6006424c8c83279dadf5b61b54
Jasdeep Singh <j.singhhh917@gmail.com>
2025-03-05 17:51:47 +1100

    refactoring callback hell

--- async.js
+++ async.js
@@ -43,13 +43,58 @@
 
 
 // same code as above but using 'try & catch" to handle errors
-async function getPlanets() {
-  try {
-    const res = await axios.get('https://swapi.co/api/planets/');
-    console.log(res.data);
-  } catch (err) {
-    console.log('In catch', err);
-  }
+// async function getPlanets() {
+//   try {
+//     const res = await axios.get('https://swapi.co/api/planets/');
+//     console.log(res.data);
+//   } catch (err) {
+//     console.log('In catch', err);
+//   }
+// }
+
+// getPlanets();
+
+// refactoring moving button
+const moveX = (element, amount, delay) => {
+  return new Promise((resolve,reject) => {
+    setTimeout(() => {
+      const bodyBoundary = document.body.clientWidth;
+      const elRight = element.getBoundingClientRect().right;
+      const currLeft = element.getBoundingClientRect().left;
+      if (elRight + amount > bodyBoundary) {
+        reject({bodyBoundary, elRight, amount})
+      }
+      else {
+        element.style.transform = `translateX(${currLeft + amount}px)`;
+        resolve();
+      }
+    }, delay);
+  })
+};
+
+const btn = document.querySelector('button');
+
+async function animateRight(el, amt) {
+  await moveX(el, amt, 1000);
+  await moveX(el, amt, 1000);
+  await moveX(el, amt, 1000);
+  await moveX(el, amt, 1000);
+  await moveX(el, amt, 1000);
+  await moveX(el, amt, 1000);
 }
 
-getPlanets();
+animateRight(btn, 100).catch((err) => {
+  console.log('NO MORE ROOM!');
+  animateRight(btn, -100);
+})
+
+// moveX(btn, 300, 1000)
+// .then(() => moveX(btn, 300, 1000))
+// .then(() => moveX(btn, 300, 1000))
+// .then(() => moveX(btn, 300, 1000))
+// .then(() => moveX(btn, 300, 1000))
+// .then(() => moveX(btn, 300, 1000))
+// .catch(({bodyBoundary, elRight, amount}) => {
+//   console.log(`Body is ${bodyBoundary}px wide`);
+//   console.log(`Element is at ${elRight}px, ${amount}px is too large!`);
+// });
